5554a1b6f8fe4e04133dcea91f24ad670cf6e511
juliana.santos <juliana.santos@db1.com.br>
2025-05-23 11:07:11 -0300

    Projeto inicial CodeCraftMentor

---
+++ src/main/java/com/graphql/customermentor/CustomermentorApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.customermentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomermentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/customermentor/CustomermentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.customermentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomermentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
